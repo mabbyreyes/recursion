5342f11e4bee770ece2db61d6c9e570210fe4886
mabbyreyes <mabbyreyes@gmail.com>
2020-02-03 11:52:36 -0700

    Display name added.

--- test/edu/cnm/deepdive/PalindromesTest.java
+++ test/edu/cnm/deepdive/PalindromesTest.java
@@ -2,18 +2,21 @@ package edu.cnm.deepdive;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
 class PalindromesTest {
 
+  @DisplayName("Test known palindromes")
   @ParameterizedTest
   @CsvFileSource(resources = "/affirmative.csv")
   void isPalindromeAffirmative(String testCase) {
     assertTrue(Palindromes.isPalindrome(testCase));
   }
 
+  @DisplayName("Test known non-palindromes")
   @ParameterizedTest
   @CsvFileSource(resources = "/negative.csv")
   void isPalindromeNegative(String testCase) {
